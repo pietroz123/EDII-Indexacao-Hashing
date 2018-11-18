feef187a4263367e18324a9604e7c38c1cf8c9d9
Pietro <Pietro@localhost.localdomain>
2018-11-18 01:02:18 -0200

    Funcao de hash: indica a posicao a inserir

--- 743588_ED2_T03A.c
+++ 743588_ED2_T03A.c
@@ -213,11 +213,24 @@ void carregar_arquivo()
     scanf("%[^\n]\n", ARQUIVO);
 }
 
-/*Auxiliar para a função de hash*/
-short f(char x)
-{
+/* ============================================================================================
+   ================================== FUNCOES DE HASH =========================================
+   ============================================================================================ */
+
+/***************************** Auxiliar para a função de hash ***********************************/
+short f(char x) {
     return (x < 59) ? x - 48 : x - 54;
 }
+
+/************************************ Funcao de Hash ********************************************/
+short hash(const char *chave, int tam) {
+    int resultado = 0;
+    for (int i = 0; i < 8; i++)
+        resultado += (i+1) * f(chave[i]);
+    return resultado % tam;
+}
+
+
 
 /* Exibe o produto */
 int exibir_registro(int rrn)    //todo
@@ -391,6 +404,12 @@ void cadastrar(Hashtable *tabela) {
 
 	// Coloca a entrada no ARQUIVO de dados
 	strcat(ARQUIVO, entrada);
+
+
+    // Procura onde inserir
+    printf("posicao: %d\n", hash(novo.pk, tabela->tam));  //!?!
+
+
 
 
 	nregistros++;
